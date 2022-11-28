f952f714a07ee0e3f8ae151d08b57870baf0b436
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2022-11-28 23:27:50 +0100

    ajout animations jquery pour la presentation

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-$("#contacterTXT")
+
+// Etat initial de la page
+$(document).ready(function() {
+    $("#Presentation").hide();
+})
+
+// Animation pour cacher/afficher l'image et le texte de présentation
+$("#titre_encadre").on("click", function () {
+    $("#Presentation").toggle(1000, "swing");
+});
