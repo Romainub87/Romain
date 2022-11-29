5fa23266e63b7edf9d379955f9823df632bbcecd
Romain Cordier <rcordier@iut.u-bordeaux.fr>
2022-11-29 17:52:27 +0100

    #

--- JS/index.js
+++ JS/index.js
@@ -7,17 +7,3 @@ $(document).on("ready", function() {
 $("#titre_encadre").on("click", function() {
     $("#Presentation").toggle(1000, "swing");
 });
-
-$("#Logo").on("click", function() {
-    $("#Logo").css({
-        "border-bottom-right-radius": "25px",
-        "padding-left": "50px",
-        "padding-right": "50px",
-
-    });
-    $("#logoEmplacement").css({
-        "width": "100%",
-        "text-align": "center",
-        "top": "0",
-    })
-});
