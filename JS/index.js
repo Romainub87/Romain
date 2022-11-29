c8d1cd3f9bd87ccee68db69a5ee3c904f16f84aa
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2022-11-30 00:03:45 +0100

    ajout coordonnées en bas de page

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,21 @@
 // Etat initial de la page
-$(document).on("ready", function() {
+$(document).on("ready", function () {
     $("#Presentation").hide();
+    $("#InfosCoord").hide();
+    $('#contacterTXT').text('Mes liens');
 });
 
 // Animation pour cacher/afficher l'image et le texte de présentation
-$("#titre_encadre").on("click", function() {
+$("#titre_encadre").on("click", function () {
     $("#Presentation").toggle(1000, "swing");
+});
+
+$("#contacterTXT").on("click", function () {
+    $(".contact").toggle(1000, "swing");
+    $("#InfosCoord").toggle(1000, "swing");
+    if ($("#contacterTXT").text() == "Mes liens") {
+        $("#contacterTXT").text("Mes coordonnées");
+    } else {
+        $('#contacterTXT').text('Mes liens');
+    }
 });
