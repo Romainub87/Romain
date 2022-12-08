94a0b91ba6d7eb1bbbe0efa3335304c6887447c5
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2022-12-09 00:39:07 +0100

    refonte site

--- JS/index.js
+++ JS/index.js
@@ -24,7 +24,6 @@ $("#contacterTXT").on("click", function () {
 // Gestion des formes géométriques sur la page
 $('.draggable').draggable();
 $('.draggable').throwable();
-$('.draggable').css({ 'z-index': -1 })
 
 var header = document.getElementById("#nav");
 
