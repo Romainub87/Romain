6c4d35a53e45fc37923919489ce6fbbb43c48e7d
rocordier <rocordier@INFO-206-29>
2022-12-09 09:42:55 +0100

    reglages draggable responsive

--- JS/index.js
+++ JS/index.js
@@ -1,35 +1,26 @@
 // Etat initial de la page
 $(document).on("ready", function () {
-    $("#Presentation").hide();
-    $("#InfosCoord").hide();
-    $('#contacterTXT').text('Mes liens');
+  $("#Presentation").hide();
+  $("#InfosCoord").hide();
+  $("#contacterTXT").text("Mes liens");
 });
 
 // Animation pour cacher/afficher l'image et le texte de présentation
 $("#titre_encadre").on("click", function () {
-    $("#Presentation").toggle(1000, "swing");
+  $("#Presentation").toggle(1000, "swing");
 });
 
 // Animation pour cacher/afficher les coordonnées et les liens
 $("#contacterTXT").on("click", function () {
-    $(".contact").toggle(1000, "swing");
-    $("#InfosCoord").toggle(1000, "swing");
-    if ($("#contacterTXT").text() == "Mes liens") {
-        $("#contacterTXT").text("Mes coordonnées");
-    } else {
-        $('#contacterTXT').text('Mes liens');
-    }
+  $(".contact").toggle(1000, "swing");
+  $("#InfosCoord").toggle(1000, "swing");
+  if ($("#contacterTXT").text() == "Mes liens") {
+    $("#contacterTXT").text("Mes coordonnées");
+  } else {
+    $("#contacterTXT").text("Mes liens");
+  }
 });
 
-// Gestion des formes géométriques sur la page
-$('.draggable').draggable();
-$('.draggable').throwable();
-
-var header = document.getElementById("#nav");
-
-function scrolled(){
-	header.className = (window.pageYOffset >= document.documentElement.clientHeight - header.offsetHeight) ? "fixed" : "";
-}
-
-addEventListener("scroll", scrolled, false);
+$(".draggable").draggable();
+$(".draggable").throwable();
 
