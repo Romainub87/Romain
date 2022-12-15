fcb7189c54fb0e2d9242addde64376804f8d11c5
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2022-12-15 17:11:10 +0100

    présentation projets

--- JS/index.js
+++ JS/index.js
@@ -3,22 +3,11 @@ $(document).on("ready", function () {
   $("#Presentation").hide();
   $(".coord").hide();
   $("#contacterTXT").text("Mes liens");
-  $(".pres-proj").hide();
 });
 
 // Animation pour cacher/afficher l'image et le texte de présentation
 $("#titre_encadre").on("click", function () {
   $("#Presentation").toggle(1000, "swing");
-});
-
-$(".image-proj").on("click", function () {
-  $(".pres-proj").show();
-  $(".image-proj").hide();
-});
-
-$(".pres-proj").on("click", function () {
-  $(".pres-proj").hide();
-  $(".image-proj").show();
 });
 
 $(".nav-btn").on("click", function () {
